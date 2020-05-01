eb018197c71c53fcff4ce1bfca0aa2ba461a1183
Judith Oiku <judithoiku@rocketmail.com>
2020-05-01 16:54:22 +0100

    added successfully to localstorage and watch for saved notes

--- src/components/noteBook.vue
+++ src/components/noteBook.vue
@@ -2,80 +2,92 @@
     <div id="notebook">
         <!-- sidebar -->
         <aside class="sidebar">
-            <button @click="addNote" :title="addNoteTitle">Add Note</button>
-            <div class="notes" v-for="(note, index) in notes" :key="index">
-                {{note.title}}
+            <button @click="addNote" 
+            :title="addNoteTitle">
+            Add Note</button>
+            <!-- note-list -->
+            <div class="notes">            
+              <div class="note" 
+              v-for="(note, index) of notes" 
+              :key="index"  @click="selectNote(note)" 
+              :class="{selected: note === selectedNote}">
+              {{note.title}}</div>           
             </div>
         </aside>
         <!-- main pane -->
-        <section class="main">
-            <textarea v-model="content"></textarea>
+        <section class="main" v-if="selectedNote">
+            <textarea v-model="selectedNote.content"></textarea>
         </section>
         <!-- preview pane -->
-        <aside class="preview" v-html="notePreview"></aside>
+        <aside class="preview"
+         v-if="selectedNote" 
+         v-html="notePreview"></aside>
     </div>
 </template>
 
 
 <script>
 import marked from "marked"
+// import styles from '../designs/styles.css'
 
     export default {
         name: 'note-book',
        
         data (){
             return{
-                content: localStorage.getItem('content') || 'You can write in **markdown**',
-                notes: [
+                
+            // content: localStorage.getItem('content') || 'You can write in **markdown**',
+               
+                notes: JSON.parse(localStorage.getItem('notes')) || [
                     {
                         id: 1,
                         title: 'vue in action',
-                        content: 'This is a **very** interesting _course_',
+                        content: "This is a **vuex** interesting _course_",
                         created: '30th, April',
                         favorite: false
                     },
                     {
                         id: 2,
                         title: 'vuex state management',
-                        content: 'This is a **very** interesting _course_',
+                        content: "This is a **very** interesting _course_",
                         created: '30th, April',
                         favorite: false
                     },
                     {
                         id: 3,
                         title: 'vue router',
-                        content: 'This is a **very** interesting _course_',
+                        content: "This is a **vue-router** interesting _course_",
                         created: '30th, April',
                         favorite: false
                     },
 
                 ],
+                selectedId: null,
             }
         },
         // created () {
         //     this.content = localStorage.getItem('content') || 'You can write in **markdown**';
         // },
         computed: {
+            notePanel() {
+                return this.selectedNote ? this.selectedNote.content : 'You can write in **markdown**'
+            },
             notePreview (){
-                return marked(this.content)
+                // return marked(this.content)
+                return this.selectedNote ? marked(this.selectedNote.content) : marked('You can write in **markdown**')
             },
             addNoteTitle() {
-                return this.notes.length + 'note (s) already'
-            }
+                return this.notes.length + 'note(s) already'
+            },
+            selectedNote () {
+                return this.notes.find(note =>note.id === this.selectedId)
+            },
         },
         methods: {
-            saveNote() {
-                 console.log('saving note:', this.content)
-                localStorage.setItem('content', this.content)
-                this.reportOperation('saving')
-            },
-            reportOperation(opName) {
-                console.log('The', opName, 'operation was completed!')
-            },
             addNote(){
                 const time = Date.now()
                 const note = {
-                    id: String,
+                    id: String(time),
                     title: 'New note' + (this.notes.length + 1),
                     content: '**Hi** !',
                     created: time,
@@ -83,10 +95,23 @@ import marked from "marked"
                 }
                 // Add to the list
                 this.notes.push(note)
+            },
+            selectNote(note) {
+             this.selectedId = note.id
+            },
+            saveNotes (){
+                //stringify to JSON before storing
+                localStorage.setItem('notes', JSON.stringify(this.notes))
+                console.log('Notes saved!', new Date())
             }
-        },
+        }, 
         watch: {
-           content: 'saveNote',
+           notes: {
+               handler:'saveNotes',
+               deep: true,
+           },
+           
+           
         },
     }
     
@@ -118,8 +143,15 @@ import marked from "marked"
     button{
         margin-bottom:20px;
     }
-    .notes {
+    .note {
         margin-top: 10px;
+        cursor: pointer;
+    }
+    .selected {
+        background-color: green;
+        color: white;
+        padding: 10px;
+        margin-right: 15px;
     }
 
 </style>
